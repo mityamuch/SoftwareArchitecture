0b60628f92ed58df04a37cd7b7a7b2251382a71e
d.mukhanov <much.mitya@gmail.com>
2025-03-30 14:22:18 +0000

    feat(budget-dynamics): и небольшие переработки сервисарасходов и доходов

--- budgeting-system/income_service/api/budget_endpoints.py
+++ budgeting-system/income_service/api/budget_endpoints.py
@@ -7,47 +7,40 @@ from Infrastructure.repositories.income_repository import IncomeRepository
 from Infrastructure.repositories.expense_repository import ExpenseRepository
 
 router = APIRouter()
-
-def get_income_repo():
-    return IncomeRepository()
-
-def get_expense_repo():
-    return ExpenseRepository()
+income_repository = IncomeRepository()
+expense_repository = ExpenseRepository()
 
 @router.post("/incomes/", response_model=Income)
 async def create_income(
     income: Income,
-    repo: IncomeRepository = Depends(get_income_repo)
 ):
-    return repo.create(income)
+    return income_repository.create(income)
 
 @router.get("/incomes/", response_model=List[Income])
-async def get_incomes(repo: IncomeRepository = Depends(get_income_repo)):
-    return repo.get_all()
+async def get_incomes():
+    return income_repository.get_all()
 
 @router.post("/expenses/", response_model=Expense)
 async def create_expense(
     expense: Expense,
-    repo: ExpenseRepository = Depends(get_expense_repo)
 ):
-    return repo.create(expense)
+    return expense_repository.create(expense)
 
 @router.get("/expenses/", response_model=List[Expense])
-async def get_expenses(repo: ExpenseRepository = Depends(get_expense_repo)):
-    return repo.get_all()
+async def get_expenses():
+    return expense_repository.get_all()
 
 @router.get("/budget-dynamics/")
 async def calculate_budget_dynamics(
     start_date: date,
     end_date: date,
-    income_repo: IncomeRepository = Depends(get_income_repo),
-    expense_repo: ExpenseRepository = Depends(get_expense_repo)
+    user_id: int
 ):
-    incomes = income_repo.get_all()
-    expenses = expense_repo.get_all()
+    incomes = income_repository.get_all()
+    expenses = expense_repository.get_all()
     
-    filtered_incomes = [income for income in incomes if start_date <= income.date <= end_date]
-    filtered_expenses = [expense for expense in expenses if start_date <= expense.date <= end_date]
+    filtered_incomes = [income for income in incomes if start_date <= income.date <= end_date and income.user_id == user_id]
+    filtered_expenses = [expense for expense in expenses if start_date <= expense.date <= end_date and expense.user_id == user_id]
 
     total_income = sum(income.amount for income in filtered_incomes)
     total_expense = sum(expense.amount for expense in filtered_expenses)
